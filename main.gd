9576e808eb6e537e52004fd2daa0e08f4137054e
Shuflduf <shuflduf@gmail.com>
2024-07-06 18:52:06 -0600

    random sliding

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ var timer: float
 
 @onready var tile_map: TileMap = $TileMap
 
-const SLIDE_CHECK = [Vector2i(-1, 1), Vector2i(1, 1)]
+const SLIDE_CHECK = Vector2i(1, 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,11 +19,17 @@ func _process(delta: float) -> void:
 				tile_map.set_cell(0, Vector2i(cell.x, cell.y + 1), 0, Vector2i.ZERO)
 				tile_map.erase_cell(0, cell)
 			else:
-				for pos in SLIDE_CHECK:
-					if !cell + pos in all_tiles:
-						tile_map.set_cell(0, cell + pos, 0, Vector2i(0, 0))
+				var dir = randi_range(0, 1)
+				dir *= 2
+				dir -= 1
+				for i in 2:
+					dir *= -1
+					if !cell + Vector2i(dir, 1) in all_tiles:
+						tile_map.set_cell(0, Vector2i(cell.x + dir, cell.y + 1), 0, Vector2i.ZERO)
 						tile_map.erase_cell(0, cell)
-					break
+						break
+						
+						
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var mouse_pos = tile_map.local_to_map(get_local_mouse_position() / tile_map.scale)
